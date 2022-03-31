4211473aa5ec4f5e5e6f2c2719e600cf5403ff64
Catarina Salvado <catarinapsalvado@gmail.com>
2022-03-31 14:34:00 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,27 @@
+let count = 67;
+
+function render(){
+let whiteBox = document.getElementById('white-square');
+whiteBox.innerHTML = `<img id='poke'src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`
+}
+
+let prev = document.getElementById('arrow-1');
+let next = document.getElementById('arrow-2');
+
+prev.onclick = function prev(){
+    if ( count > 1){
+    count= count-1;
+    render(); 
+    }
+    
+}
+
+next.onclick = function next(){
+    if(count > 650 ){
+    count = count +1;
+    render()    
+    }
+    
+}
+
+render()
